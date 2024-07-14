c1a8e118bc9ef342951e2602eba822c1cc6283d1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-14 23:58:36 +0800

    refactor: remove unused const

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { Main } from './main'
 
-const container = document.querySelector('#root')!
-const root = createRoot(container)
-
-root.render(<Main />)
+createRoot(document.getElementById('root')!)
+  .render(<Main />)
