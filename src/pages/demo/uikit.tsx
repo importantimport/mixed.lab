807f0e1c36b1e9da67eaef6c424e101c40fd25d6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-17 23:17:20 +0800

    fix: fix uikit demo

--- src/pages/demo/uikit.tsx
+++ src/pages/demo/uikit.tsx
@@ -1,9 +1,9 @@
-import { Container } from '@react-three/uikit'
+import { Container, Fullscreen } from '@react-three/uikit'
 
 /** Example from {@link https://docs.pmnd.rs/uikit/getting-started/introduction} */
 export const UikitDemo = () => (
-  <>
+  <Fullscreen flexDirection="row" padding={10} gap={10}>
     <Container flexGrow={1} backgroundOpacity={0.5} hover={{ backgroundOpacity: 1 }} backgroundColor="red" />
     <Container flexGrow={1} backgroundOpacity={0.5} hover={{ backgroundOpacity: 1 }} backgroundColor="blue" />
-  </>
+  </Fullscreen>
 )
