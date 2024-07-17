5846aba6ad413a42a9009ab39f447689a4f6df80
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-17 23:15:50 +0800

    fix: remove fullscreen component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { Canvas } from '@react-three/fiber'
-import { Fullscreen } from '@react-three/uikit'
 import { Route, Switch } from 'wouter'
 import { XR, createXRStore } from '@react-three/xr'
 
@@ -13,14 +12,12 @@ const store = createXRStore()
 export const Main = () => (
   <Canvas gl={{ localClippingEnabled: true }}>
     <XR store={store}>
-      <Fullscreen flexDirection="row" padding={10} gap={10}>
-        <Switch>
-          <Route path="/demo/fiber" component={FiberDemo} />
-          <Route path="/demo/uikit" component={UikitDemo} />
-          <Route path="/demo/xr" component={XRDemo} />
-          <Route path="/" component={Index} />
-        </Switch>
-      </Fullscreen>
+      <Switch>
+        <Route path="/demo/fiber" component={FiberDemo} />
+        <Route path="/demo/uikit" component={UikitDemo} />
+        <Route path="/demo/xr" component={XRDemo} />
+        <Route path="/" component={Index} />
+      </Switch>
     </XR>
   </Canvas>
 )
