14acf09e8004d09b60ffa102aee1ecdc4acbd17c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-19 23:40:00 +0800

    chore: setup pwa-assets

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,10 +1,18 @@
 import {
   defineConfig,
-  minimal2023Preset as preset,
+//   minimal2023Preset,
 } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
   headLinkOptions: { preset: '2023' },
-  preset,
+  preset: {
+    // ...minimal2023Preset,
+    transparent: {
+      sizes: [],
+      favicons: [[48, 'favicon.ico']],
+    },
+    maskable: { sizes: [] },
+    apple: { sizes: [] },
+  },
   images: 'public/favicon.svg',
 })
