ebb6f1f7b99031f3a0fa0e08edf8b06c57a5b70f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-27 21:34:16 +0800

    refactor(header): check session mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Container, Fullscreen, Text } from '@react-three/uikit'
 import { Button, Defaults } from '@react-three/uikit-default'
 import { FlaskConical, Moon, Sun, SunMoon } from '@react-three/uikit-lucide'
-import { useXRStore } from '@react-three/xr'
+import { IfInSessionMode, useXRStore } from '@react-three/xr'
 
 import { useColorScheme } from '../hooks/use-color-scheme'
 import { Menubar, MenubarMenu, MenubarTrigger } from './default/menubar'
@@ -11,44 +11,46 @@ export const Header = () => {
   const { colorScheme, setColorScheme } = useColorScheme()
 
   return (
-    <Fullscreen flexDirection="column">
-      <Defaults>
-        <Menubar borderRadius={0} paddingX={8} lg={{ paddingX: 16 }}>
-          <MenubarMenu>
-            <MenubarTrigger>
-              <FlaskConical paddingX={4} width={24} />
-              <Text>Mixed.Lab</Text>
-            </MenubarTrigger>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger onClick={() => store.enterAR()}>
-              <Text>Enter AR</Text>
-            </MenubarTrigger>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger onClick={() => store.enterVR()}>
-              <Text>Enter VR</Text>
-            </MenubarTrigger>
-          </MenubarMenu>
-          <Container flexGrow={1} />
-          <MenubarMenu>
-            <Button
-              variant="ghost"
-              size="sm"
-              onClick={() => window.open('https://github.com/importantimport/mixed.lab', '_blank')}
-            >
-              <Text>Source Code</Text>
-            </Button>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger
-              onClick={() => setColorScheme(colorScheme === 'light' ? 'dark' : colorScheme === 'dark' ? 'system' : 'light')}
-            >
-              {colorScheme === 'dark' ? <Moon /> : colorScheme === 'system' ? <SunMoon /> : <Sun />}
-            </MenubarTrigger>
-          </MenubarMenu>
-        </Menubar>
-      </Defaults>
-    </Fullscreen>
+    <IfInSessionMode deny={['immersive-ar', 'immersive-vr']}>
+      <Fullscreen flexDirection="column">
+        <Defaults>
+          <Menubar borderRadius={0} paddingX={8} lg={{ paddingX: 16 }}>
+            <MenubarMenu>
+              <MenubarTrigger>
+                <FlaskConical paddingX={4} width={24} />
+                <Text>Mixed.Lab</Text>
+              </MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger onClick={() => store.enterAR()}>
+                <Text>Enter AR</Text>
+              </MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger onClick={() => store.enterVR()}>
+                <Text>Enter VR</Text>
+              </MenubarTrigger>
+            </MenubarMenu>
+            <Container flexGrow={1} />
+            <MenubarMenu>
+              <Button
+                variant="ghost"
+                size="sm"
+                onClick={() => window.open('https://github.com/importantimport/mixed.lab', '_blank')}
+              >
+                <Text>Source Code</Text>
+              </Button>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger
+                onClick={() => setColorScheme(colorScheme === 'light' ? 'dark' : colorScheme === 'dark' ? 'system' : 'light')}
+              >
+                {colorScheme === 'dark' ? <Moon /> : colorScheme === 'system' ? <SunMoon /> : <Sun />}
+              </MenubarTrigger>
+            </MenubarMenu>
+          </Menubar>
+        </Defaults>
+      </Fullscreen>
+    </IfInSessionMode>
   )
 }
