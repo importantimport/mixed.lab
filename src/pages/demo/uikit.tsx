fbba5c29ed8d761a51f7b796ca2b278f6aa91ca7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-27 23:20:58 +0800

    fix: uikit demo

--- src/pages/demo/uikit.tsx
+++ src/pages/demo/uikit.tsx
@@ -1,9 +1,16 @@
-import { Container, Fullscreen } from '@react-three/uikit'
+import { Container } from '@react-three/uikit'
+import { useTunnel } from '../../contexts/tunnel'
 
 /** Example from {@link https://docs.pmnd.rs/uikit/getting-started/introduction} */
-export const UikitDemo = () => (
-  <Fullscreen flexDirection="row" padding={10} gap={10}>
-    <Container flexGrow={1} backgroundOpacity={0.5} hover={{ backgroundOpacity: 1 }} backgroundColor="red" />
-    <Container flexGrow={1} backgroundOpacity={0.5} hover={{ backgroundOpacity: 1 }} backgroundColor="blue" />
-  </Fullscreen>
-)
+export const UikitDemo = () => {
+  const tunnel = useTunnel()
+
+  return (
+    <tunnel.In>
+      <Container height="100%" width="100%" flexDirection="row" padding={10} gap={10}>
+        <Container flexGrow={1} backgroundOpacity={0.5} hover={{ backgroundOpacity: 1 }} backgroundColor="red" />
+        <Container flexGrow={1} backgroundOpacity={0.5} hover={{ backgroundOpacity: 1 }} backgroundColor="blue" />
+      </Container>
+    </tunnel.In>
+  )
+}
