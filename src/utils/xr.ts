03b22d4358f9438fdbe9918bfe83d44dc2ca88a3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-02 22:44:08 +0800

    chore(xr): white hand

--- src/utils/xr.ts
+++ src/utils/xr.ts
@@ -3,8 +3,8 @@ import type { XRStoreOptions } from '@react-three/xr'
 export const options = {
   controller: { teleportPointer: true },
   hand: {
-    rayPointer: { cursorModel: { color: 'black' } },
+    rayPointer: { cursorModel: { color: 'white' } },
     teleportPointer: true,
-    touchPointer: { cursorModel: { color: 'black' } },
+    touchPointer: { cursorModel: { color: 'white' } },
   },
 } as const satisfies XRStoreOptions
