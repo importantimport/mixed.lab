f55904b8c2c880a03b3dd9aa2778ee3fd1421835
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-02 22:28:14 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 import antfu from '@antfu/eslint-config'
+import ii from '@importantimport/eslint-config'
 
 export default antfu({
   formatters: true,
   react: true,
-}, {
-  rules: {
-    'antfu/top-level-function': 'off',
-  },
-})
+  typescript: true,
+}).append(ii({ functional: false }))
